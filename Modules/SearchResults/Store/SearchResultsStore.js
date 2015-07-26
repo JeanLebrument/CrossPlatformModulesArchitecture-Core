39ec012adae0d9fed1b24117aea6a47ab0b85428
Jean Lebrument <Jean.Lebrument@gmail.com>
2015-07-26 11:11:03 +0200

    Refacto Store

--- Modules/SearchResults/Store/SearchResultsStore.js
+++ Modules/SearchResults/Store/SearchResultsStore.js
@@ -4,24 +4,12 @@ var EventEmitter = require('events').EventEmitter;
 var AppDispatcher = require('../../../Common/AppDispatcher');
 var SearchResultsConstants = require('../Constants/SearchResultsConstants');
 
-var _results = null;
-var _resultError = null;
-
-function _setResults(results) {
-  _results = results;
-}
-
-function _setResultError(resultError) {
-  _resultError = resultError;
-}
-
 class SearchResultsStore extends EventEmitter {
-  getResults() {
-    return _results;
-  }
-
-  getResultError() {
-    return _resultError;
+  constructor() {
+    super();
+    
+    this.results = null;
+    this.resultError = null;
   }
 
   emitChange() {
@@ -42,8 +30,8 @@ var searchResultsStore = new SearchResultsStore();
 AppDispatcher.register(function(payload) {
   switch (payload.eventName) {
     case SearchResultsConstants.SEARCH_FOR_LOCATION:
-      _setResults(payload.results);
-      _setResultError(payload.resultError);
+      searchResultsStore.results = payload.results;
+      searchResultsStore.resultError = payload.resultError;
       break;
   }
 
